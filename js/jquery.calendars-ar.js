8c80961416f5492d992af7b2e79dcf4d476bf1c6
Ala Obeidat <ala-work@hotmail.com>
2020-06-04 02:07:52 +0300

    Upload files

---
+++ js/jquery.calendars-ar.js
@@ -0,0 +1,26 @@
+﻿/* http://keith-wood.name/calendars.html
+   Arabic localisation for Gregorian/Julian calendars for jQuery.
+   Khaled Al Horani -- خالد الحوراني -- koko.dw@gmail.com. */
+/* NOTE: monthNames are the original months names and they are the Arabic names,
+   not the new months name فبراير - يناير and there isn't any Arabic roots for these months */
+(function ($) {
+    if (_datePickerLang == "ar") {
+        $.calendars.calendars.gregorian.prototype.regionalOptions['ar'] = {
+            name: 'Gregorian',
+            epochs: ['BCE', 'CE'],
+            monthNames: ['كانون الثاني', 'شباط', 'آذار', 'نيسان', 'آذار', 'حزيران',
+            'تموز', 'آب', 'أيلول', 'تشرين الأول', 'تشرين الثاني', 'كانون الأول'],
+            monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+            dayNames: ['الأحد', 'الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'],
+            dayNamesShort: ['ح', 'ن', 'ث', 'ر', 'خ', 'ج', 'س'],
+            dayNamesMin: ['ح', 'ن', 'ث', 'ر', 'خ', 'ج', 'س'],
+            dateFormat: 'dd/mm/yyyy',
+            firstDay: 6,
+            isRTL: true
+        };
+        if ($.calendars.calendars.julian) {
+            $.calendars.calendars.julian.prototype.regionalOptions['ar'] =
+                $.calendars.calendars.gregorian.prototype.regionalOptions['ar'];
+        }
+    }
+})(jQuery);
---
+++ js/script-ar.js
@@ -0,0 +1,20 @@
+var _datePickerLang = "ar"; // ="en"
+ var _datepickerChange;
+        var _datePickerType = "ummalqura"; 
+        var _datepickerChange = null;
+		var _gregorianDatepicker;
+    var _gregorianDatePickerType = "gregorian";
+    var _gregorianDatepickerChange = null;
+		$(function () {
+        //	$.calendars.picker.setDefaults({renderer: $.calendars.picker.themeRollerRenderer}); // Requires jquery.calendars.picker.ext.js
+        var calendar = $.calendars.instance(_datePickerType, _datePickerLang);
+        $('.datepicker').calendarsPicker({
+            calendar: calendar,
+            onSelect: function (newDate) { if (_datepickerChange) _datepickerChange(newDate); }
+        });
+        var gregorianCalendar = $.calendars.instance(_gregorianDatePickerType, _datePickerLang);
+        $('.gregorianDatepicker').calendarsPicker({
+            calendar: gregorianCalendar,
+            onSelect: function (newDate) { if (_gregorianDatepickerChange) _gregorianDatepickerChange(newDate); }
+        });
+    });
